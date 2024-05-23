23bb99eda9091cd99930b462fadc4a94d1800954
juan9walteros <80785635+Juan9Walteros@users.noreply.github.com>
2024-05-22 21:44:02 -0500

    Cambios relacionados al fronted

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import store from "./store";
 
-import Error404 from "./containers/errors/Error404";
-import Home from "./containers/pages/Home";
+
 import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
-import About from "containers/pages/About";
-import Destination from "containers/pages/Destination";
-import Tour from "containers/pages/Tour";
-import Blog from "containers/pages/Blog";
+
+import AnimatedRoutes from "./AnimatedRoutes";
 
 function App() {
+
   return (
     <Provider store={store}>
       <Router>
-        <Routes>
-          <Route path="*" element={<Error404 />} />
-
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Destination" element={<Destination />} />
-          <Route path="/Tour" element={<Tour />} />
-          <Route path="/Blog" element={<Blog />} />
-
-        </Routes>
+        <AnimatedRoutes />
       </Router>
     </Provider>
   );
--- src/containers/pages/Tour.jsx
+++ src/containers/pages/Tour.jsx
@@ -1,18 +1,195 @@
- 
-import Footer from "components/navigation/Footer"
-import Navbar from "components/navigation/Navbar"
-import Layout from "hocs/layouts/Layout"
+import Footer from "components/navigation/Footer";
+import Navbar from "components/navigation/Navbar";
+import Layout from "hocs/layouts/Layout";
 
-    function Tour(){
-        return(
-            <Layout>
-                <Navbar />
-                <div className="pt-28">
-                    Tour
+import Roma from "assets/img/rome.png";
+import London from "assets/img/london2.png";
+import Osaka from "assets/img/osaka.png";
+import Palmas from "assets/img/Plameras ne.png";
+
+
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import { Card, CardBody, CardTitle } from "react-bootstrap";
+
+function Tour() {
+  return (
+    <Layout>
+      <Navbar />
+      <div className="ContTx">
+        <div className="ConTx">
+          <img className="Palmas" src={Palmas} alt="..." />
+          <div className="txtexclusive2">
+            <div className="title3">
+              <h1 className="txtE3">
+                Best <span>vacation plan</span>
+              </h1>
+            </div>
+            <div className="ContTx">
+              <p className="txtD3">
+                Plan your perfect vacation with our travel agency. Chose <br />
+                amoung hundresds of all-inclusive offers!
+              </p>
+            </div>
+          </div>
+          <Row>
+            <Col className="col-4">
+              <div>
+                <div>
+                  <Card className="card3">
+                    <Card.Img
+                      className="img-fluid"
+                      src={Roma}
+                      alt="..."
+                      style={{
+                        width: "369px",
+                        height: "327px",
+                        borderRadius: "16px",
+                      }}
+                    />
+                    <CardBody>
+                      <div className="container">
+                        <div className="row">
+                          <div className="col-6">
+                            <CardTitle className="cardT1">
+                              Rome, Itaky
+                            </CardTitle>
+                          </div>
+                          <div className="col-6">
+                            <p className="textT1">$5,42k</p>
+                          </div>
+                          <div className="col-6">
+                            <p className="card-text">
+                              <i class="bi bi-cursor-fill"></i>
+                              <small className="textm text-body-secondary">
+                                {" "}
+                                10 Days Trip
+                              </small>
+                            </p>
+                          </div>
+                          <div className="col-6">
+                            <p className="card-text">
+                              <small className="text-body-secondary">
+                                <i
+                                  class="bi bi-star-fill"
+                                  style={{ color: "#ffc107" }}
+                                ></i>{" "}
+                                4.8
+                              </small>
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </CardBody>
+                  </Card>
                 </div>
-                <Footer />
+              </div>
+            </Col>
+            <Col className="col-4">
+              <div>
+                <Card className="card3">
+                  <Card.Img
+                    className="img-fluid"
+                    src={London}
+                    alt="..."
+                    style={{
+                      width: "369px",
+                      height: "327px",
+                      borderRadius: "16px",
+                    }}
+                  />
+                  <CardBody>
+                    <div className="container">
+                      <div className="row">
+                        <div className="col-6">
+                          <CardTitle className="cardT1">London, UK</CardTitle>
+                        </div>
+                        <div className="col-6">
+                          <p className="textT1">$2,42k</p>
+                        </div>
+                        <div className="col-6">
+                          <p className="card-text">
+                            <i class="bi bi-cursor-fill"></i>
+                            <small className="textm text-body-secondary">
+                              {" "}
+                              07 Days Trip
+                            </small>
+                          </p>
+                        </div>
+                        <div className="col-6">
+                          <p className="card-text">
+                            <small className="text-body-secondary">
+                              <i
+                                class="bi bi-star-fill"
+                                style={{ color: "#ffc107" }}
+                              ></i>{" "}
+                              4.7
+                            </small>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </CardBody>
+                </Card>
+              </div>
+            </Col>
+            <Col className="col-4">
+              <div>
+                <Card className="card3">
+                  <Card.Img
+                    className="img-fluid"
+                    src={Osaka}
+                    alt="..."
+                    style={{
+                      width: "369px",
+                      height: "327px",
+                      borderRadius: "16px",
+                    }}
+                  />
+                  <CardBody>
+                    <div className="container">
+                      <div className="row">
+                        <div className="col-6">
+                          <CardTitle className="cardT1">Osaka,Japan</CardTitle>
+                        </div>
+                        <div className="col-6">
+                          <p className="textT1">$5,42k</p>
+                        </div>
+                        <div className="col-6">
+                          <p className="card-text">
+                            <i class="bi bi-cursor-fill"></i>
+                            <small className="textm text-body-secondary">
+                              {" "}
+                              10 Days Trip
+                            </small>
+                          </p>
+                        </div>
+                        <div className="col-6">
+                          <p className="card-text">
+                            <small className="text-body-secondary">
+                              <i
+                                class="bi bi-star-fill"
+                                style={{ color: "#ffc107" }}
+                              ></i>{" "}
+                              4.8
+                            </small>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </CardBody>
+                </Card>
+              </div>
+            </Col>
+          </Row>
+          
+        </div>
        
-            </Layout>
-        )
-    }
-    export default Tour
+      </div>
+
+      <Footer />
+    </Layout>
+  );
+}
+export default Tour;
