d1957b069b1062fc0433eb4358056ef7ec4856d6
juan9walteros <80785635+Juan9Walteros@users.noreply.github.com>
2024-05-24 19:45:29 -0500

    Cambios sobre cosas relacionadas al blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
+def blog_thumbnail_directory(instance, filename):
+    return'blog/{0}/{1}'
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    thumbnail = models.ImageField(upload_to='blog')
